b401969419fe8d4d22b02d520a4e49590f1191f2
Rahmoni <ghedjatirahmoni@gmail.com>
2025-12-20 19:37:31 +0100

    adding find by Domain function

--- src/main/java/com/h80/demo/Repository/MongoRepo.java
+++ src/main/java/com/h80/demo/Repository/MongoRepo.java
@@ -1,9 +1,13 @@
 package com.h80.demo.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.h80.demo.Document.Servers;
 
 public interface  MongoRepo extends MongoRepository<Servers, String> {
     boolean existsByDomain(String domain); 
+
+    Optional<Servers> findByDomain(String domain); 
 }
