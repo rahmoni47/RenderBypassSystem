8b8807427e7318a266319446fcc6a266a6fb5f2f
Rahmoni <ghedjatirahmoni@gmail.com>
2025-12-20 19:23:37 +0100

    creating a function to check if a server already exist in db using Derived Query Methods

--- src/main/java/com/h80/demo/Repository/MongoRepo.java
+++ src/main/java/com/h80/demo/Repository/MongoRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.h80.demo.Document.Servers;
 
 public interface  MongoRepo extends MongoRepository<Servers, String> {
-
+    boolean existsByDomain(String domain); 
 }
